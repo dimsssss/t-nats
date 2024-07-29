9cba6ae421963bfb97821cb4a106e11d10421be9
dev <caruen@hanmail.net>
2024-07-29 20:57:02 +0900

    feat: client, observer

---
+++ src/Observer.ts
@@ -0,0 +1,51 @@
+import { Client } from './Client';
+
+export class Observer {
+  private subjects: Record<string, Record<string, Client>[]> = {};
+  private queueGroup: Record<string, Client[]> = {};
+  private clientIds: Record<string, string> = {};
+
+  constructor() {}
+
+  addClient(client: Client, args: string[]) {
+    const [subject, group, sid] = args;
+
+    if (this.clientIds[sid]) {
+      throw new Error('sid should be unique');
+    }
+
+    if (group) {
+      this.queueGroup[group].push(client);
+    }
+
+    this.subjects[subject].push({ sid: client });
+  }
+
+  publish(args: string[]) {
+    const [subject, replyTo, bytesSize, payload] = args;
+
+    if ((!replyTo && !payload) || Number(bytesSize) === 0) {
+      this.subjects[subject].forEach((value) => {
+        Object.values(value).forEach((client: Client) => {
+          client.send('');
+        });
+      });
+    } else if (!replyTo && payload) {
+      this.subjects[subject].forEach((value) => {
+        Object.values(value).forEach((client: Client) => {
+          client.send(payload);
+        });
+      });
+    } else if (replyTo && payload) {
+      Object.entries(this.subjects[subject])
+        .filter(([key, client]) => {
+          return key === replyTo;
+        })
+        .forEach((key, value) => {
+          Object.values(value).forEach((client: Client) => {
+            client.send(payload);
+          });
+        });
+    }
+  }
+}
