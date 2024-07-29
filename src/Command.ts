a17a51754a5d32ef4867c741276b6b442b24aa9e
dev <caruen@hanmail.net>
2024-07-29 13:38:32 +0900

    feat: parser

---
+++ src/Command.ts
@@ -0,0 +1,60 @@
+const C_C = 'c';
+const C_CO = 'co';
+const C_CON = 'con';
+const C_CONN = 'conn';
+const C_CONNE = 'conne';
+const C_CONNEC = 'connec';
+const C_CONNECT = 'connect';
+const C_CONNECT_ARG = 'c_connect_arg';
+
+const C_PAYLOAD = 'c_payload';
+const C_PAYLOAD_RE = 'c_payload_r_e';
+const C_PAYLOAD_NE = 'c_payload_n_e';
+
+const C_P = 'p';
+const C_PI = 'pi';
+const C_PIN = 'pin';
+const C_PING = 'ping';
+
+const C_PO = 'po';
+const C_PON = 'pon';
+const C_PONG = 'pong';
+
+const C_PU = 'pu';
+const C_PUB = 'pub';
+const C_PUB_ARG = 'c_pub_arg';
+const C_PUB_MSG = 'c_pub_msg';
+
+const C_S = 's';
+const C_SU = 'su';
+const C_SUB = 'sub';
+const C_SUB_ARG = 'c_sub_arg';
+
+export {
+  C_C,
+  C_CO,
+  C_CON,
+  C_CONN,
+  C_CONNE,
+  C_CONNEC,
+  C_CONNECT,
+  C_CONNECT_ARG,
+  C_PAYLOAD,
+  C_PAYLOAD_RE,
+  C_PAYLOAD_NE,
+  C_P,
+  C_PI,
+  C_PIN,
+  C_PING,
+  C_PO,
+  C_PU,
+  C_PUB,
+  C_PUB_ARG,
+  C_PUB_MSG,
+  C_PON,
+  C_PONG,
+  C_S,
+  C_SU,
+  C_SUB,
+  C_SUB_ARG,
+};
