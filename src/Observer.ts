669114ae7e8100a10dd0ccd9e7f11f72e75aa09e
dev <caruen@hanmail.net>
2024-07-30 23:16:33 +0900

    fix: 잘못된 publish 수정

--- src/Observer.ts
+++ src/Observer.ts
@@ -8,48 +8,62 @@ export class Observer {
   constructor() {}
 
   addClient(client: Client, args: string[]) {
-    const [subject, group, sid] = args;
+    let subject, group, sid;
+
+    if (args.length === 3) {
+      subject = args[0];
+      group = args[1];
+      sid = args[2];
+
+      this.queueGroup[group].push(client);
+    } else if (args.length === 2) {
+      subject = args[0];
+      sid = args[1];
+    } else {
+      throw new Error('invalid args length');
+    }
 
     if (this.clientIds[sid]) {
       throw new Error('sid should be unique');
-    }
-
-    if (args.length === 3) {
-      this.queueGroup[group].push(client);
     }
 
     if (!this.subjects[subject]) {
       this.subjects[subject] = [];
     }
 
-    this.subjects[subject].push({ sid: client });
+    this.subjects[subject].push({ [sid]: client });
   }
 
   publish(args: string[]) {
-    const [subject, replyTo, bytesSize, payload] = args;
-
-    if ((!replyTo && !payload) || Number(bytesSize) === 0) {
+    const [subject] = args;
+    if (args.length === 4) {
+      this.subjects[subject]
+        .filter((value) => {
+          const [sid] = Object.keys(value);
+          const replyTo = args[1];
+          return sid === replyTo;
+        })
+        .forEach((value) => {
+          Object.values(value).forEach((client: Client) => {
+            const payload = args[3];
+            client.send(`${payload}\r\n`);
+          });
+        });
+    } else if (args.length === 3) {
+      this.subjects[subject].forEach((value) => {
+        Object.values(value).forEach((client: Client) => {
+          const payload = args[2];
+          client.send(`${payload}\r\n`);
+        });
+      });
+    } else if (args.length === 2) {
       this.subjects[subject].forEach((value) => {
         Object.values(value).forEach((client: Client) => {
           client.send('');
         });
       });
-    } else if (!replyTo && payload) {
-      this.subjects[subject].forEach((value) => {
-        Object.values(value).forEach((client: Client) => {
-          client.send(payload);
-        });
-      });
-    } else if (replyTo && payload) {
-      Object.entries(this.subjects[subject])
-        .filter(([key, client]) => {
-          return key === replyTo;
-        })
-        .forEach((key, value) => {
-          Object.values(value).forEach((client: Client) => {
-            client.send(payload);
-          });
-        });
+    } else {
+      throw new Error('invalid argument length');
     }
   }
 }
